72a5ed662c7530af5dbae35ec9e5d4b83fc4efea
Nguyen <spartanb174@gmail.com>
2018-11-27 15:52:10 -0500

    Spring boot initializer
    spring boot init

---
+++ pendragon-spring/src/main/java/practice/Pendragon/PendragonApplication.java
@@ -0,0 +1,12 @@
+package practice.Pendragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PendragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PendragonApplication.class, args);
+	}
+}
